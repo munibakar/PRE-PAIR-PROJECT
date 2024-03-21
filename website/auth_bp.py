14cbdfeaea6303f66af4dde05ddc38466967fd29
münibakar <125873531+munibakar@users.noreply.github.com>
2024-03-21 14:11:57 +0300

    Update auth_bp.py

--- website/auth_bp.py
+++ website/auth_bp.py
@@ -9,14 +9,7 @@ import random
 
 db = firestore.client()
 config = {
-    'apiKey': "AIzaSyBUVGM_OnNbu_K8lLykM5T-Tu31mIUAgkw",
-    'authDomain': "prepair-demo.firebaseapp.com",
-    'projectId': "prepair-demo",
-    'storageBucket': "prepair-demo.appspot.com",
-    'messagingSenderId': "718275532446",
-    'appId': "1:718275532446:web:0f85dd84fb87cc83fd0d8f",
-    'measurementId': "G-3DLX53HNSV",
-    'databaseURL' : ''
+    '''API KEY HERE'''
 }
 
 firebase = pyrebase.initialize_app(config)
