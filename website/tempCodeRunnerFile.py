bf3d848143fa1e99b0833e424cc77780a49d3055
münibakar <125873531+munibakar@users.noreply.github.com>
2024-03-21 14:14:53 +0300

    Update tempCodeRunnerFile.py

--- website/tempCodeRunnerFile.py
+++ website/tempCodeRunnerFile.py
@@ -7,14 +7,7 @@ from firebase_admin import credentials, auth
 from flask_session import Session
 
 config = {
-    'apiKey': "AIzaSyBUVGM_OnNbu_K8lLykM5T-Tu31mIUAgkw",
-    'authDomain': "prepair-demo.firebaseapp.com",
-    'projectId': "prepair-demo",
-    'storageBucket': "prepair-demo.appspot.com",
-    'messagingSenderId': "718275532446",
-    'appId': "1:718275532446:web:0f85dd84fb87cc83fd0d8f",
-    'measurementId': "G-3DLX53HNSV",
-    'databaseURL' : ''
+'''API KEY HERE'''
 }
 
 firebase = pyrebase.initialize_app(config)
